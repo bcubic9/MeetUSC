36b0357367d801a29d2f1e49279856abc1ece094
bcubic9 <bcubic@email.sc.edu>
2022-05-31 13:49:54 -0400

    new push

--- MeetUSCFacade.java
+++ MeetUSCFacade.java
@@ -1,9 +1,52 @@
 import java.util.ArrayList;
 
-class MeetUSCFacade {
- 
-    public ArrayList<Group> displayFavoriteGroups() {
+class MeetUSCFacade 
+{
+    private boolean addUser (User user)
+    {
 
+    }
+
+    public ArrayList<Group> getFavGroups()
+    {
         return null;
     }
+
+    public ArrayList<Event> getFavEvents()
+    {
+        return null;
+    }
+
+    public boolean login(User username, String password)
+    {
+        return false;
+    }
+
+    public Event searchForEvent(Event event)
+    {
+        return event;
+    }
+
+    public Group searchForGroup(Group group)
+    {
+        return group;
+    }
+
+    public ArrayList<String> getHomeScreenInfo()
+    {
+        return null;
+    }
+
+    public ArrayList<String> getEventPageInfo(Event event)
+    {
+        return null;
+    }
+
+    public ArrayList<String> getGroupPageInfo(Group group)
+    {
+        return null;
+    }
+
+    public ArrayList<String>
+
 }
