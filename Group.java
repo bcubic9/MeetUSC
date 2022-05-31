156b55262c82f65daaf15ba7dd4341f6ba5c480f
Denise Garcia Mesa <DG27@email.sc.edu>
2022-05-31 13:54:49 -0400

    Created GroupList class and GroupList attributes

--- Group.java
+++ Group.java
@@ -1,5 +1,27 @@
-package meetUSC;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class Group {
+
+    private ArrayList<Category> category;
+    private ArrayList<User> members;
+    private ArrayList<Event> events;
+    private String name;
+    private String shortDescription;
+    private String longDescription;
+    private int groupSize;
+    private Author author;
+    private ArrayList<String> chat;
+    private User authorContact;
+    private ArrayList<Messsage> messages;
+
     
+
+    
+
+
+
+
 }
---
+++ GroupList.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+public class GroupList {
+
+    private ArrayList<Group> allGroups;
+    private ArrayList<GroupList> eventList;
+    
+
+    public GroupList(){
+
+
+    }
+    
+    public Group getInstance(){
+        return null;
+
+    }
+
+    private ArrayList<Group> getGroups(){
+        return allGroups;
+
+
+    }
+
+    private void addGroup(Group group){
+
+    }
+
+    private Group addGroup(Group group){
+
+    }
+
+    private Group editGroup(Group group){
+        return group;
+
+    }
+
+    private Group removeGroup(Group group){
+        return group;
+
+    }
+
+    private Group searchForGroup(Group group){
+        return group;
+
+    }
+
+    private ArrayList<Group> getFavoriteGroup(){
+        return allGroups;
+        
+    }
+
+    private void addFavoriteGroup(Group group){
+
+    }
+
+
+
+}
