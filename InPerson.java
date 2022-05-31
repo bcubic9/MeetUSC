ff423d95fd821c50359b2712d48b49518889650e
AaronK <alkeys89@gmail.com>
2022-05-31 18:06:47 -0400

    added a few more  classes and fixed some bugs

--- InPerson.java
+++ InPerson.java
@@ -1,14 +1,15 @@
+import java.util.Date;
 public class InPerson {
 private Location location;
-private DateTime eDate;
+private Date eDate;
 private int numOfAttendees;
 private User getAttendees(){
-
+    
 }
 private void addAttendee(User  attendee){
 
 }
-private String toString(){
+public String toString(){
     return "";
 }
 }
