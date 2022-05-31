b612c93abc4f795078cd556a472e8a24decec109
bcubic9 <bcubic@email.sc.edu>
2022-05-31 18:24:35 -0400

    Filled out Message.java

--- Message.java
+++ Message.java
@@ -1,3 +1,28 @@
-public class Message {
-    
+import java.util.*;
+
+public class Message 
+{
+    private User author;
+    private String text;
+    private Date date;
+
+    private void registerObserver(Observer observer)
+    {
+
+    }
+
+    private void removeObserver(Observer observer)
+    {
+
+    }
+
+    private void notifyObserver()
+    {
+
+    }
+
+    private void sendMessage(String message)
+    {
+        
+    }
 }
