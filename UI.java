80da93adae3929618ecd31b7541ffe65f8d160a5
bcubic9 <bcubic@email.sc.edu>
2022-05-31 13:48:28 -0400

    Brennen's push

---
+++ UI.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+public class UI 
+{
+   public String username;
+   private String password;
+   public Character guestLogin; 
+   public boolean exitApp;
+
+   public ArrayList<Group> displayFavoriteGroups()
+   {
+        ArrayList<Group> favorites = new ArrayList<Group>();
+        return favorites;
+   }
+
+   public ArrayList<Event> displayFavoriteEvents()
+   {
+       ArrayList<Event> favorites = new ArrayList<Event>();
+       return favorites;
+   }
+
+   public ArrayList<String> displayEventPage(Event event)
+   {
+       this.event = event;
+       ArrayList<String> eventPage = new ArrayList<String>(event);
+       return eventPage;
+   }
+
+   public ArrayList<String> displayGroupPage(Group group)
+   {
+       ArrayList<String> groupPage = new ArrayList<String>();
+       return groupPage;
+   }
+
+   public ArrayList<String> displayUserPage(User user)
+   {
+       ArrayList<String> userPage = new ArrayList<String>();
+       return userPage;
+   }
+
+   public void exitSystem(boolean exit)
+   {
+       
+   }
+}
