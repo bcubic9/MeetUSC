9e5a50c1b9f5ecd382556c2d91ba1af7babcacbf
AaronK <alkeys89@gmail.com>
2022-05-31 19:01:36 -0400

    JsonConstants.java additions

--- JsonConstants.java
+++ JsonConstants.java
@@ -1,6 +1,27 @@
 public abstract class JsonConstants {
 
     // Event constants
-    public static final String EVENTS_ID = "id";
-    public static final String EVENTS_NAME = "name";
+    public static final String  EVENTS_ID = "840b1734-dd18-11ec-9d64-0242ac120002";
+    public static final String  EVENTS_NAME = "Axe Throwing";
+    public static final String  EVENTS_ADDRESS = "700 Gervais St b2, Columbia, SC 29201";
+    public static final int     EVENT_SIZE = 10;
+    public static final boolean EVENT_IN_PERSON = true;
+    public static final String  EVENT_DESCRIPTION = "Aim for the head!";
+    
+    public static final String      USER_ID = "fd2a1d6d-3419-401c-a021-cbd2b444d86c";
+    public static final String      USER_PASSWORD = "qwertyPASS";
+    public static final String      USER_DISPLAY_NAME = "JohnS00";
+    public static final String      USER_GENDER = "Male";
+    public static final int         USER_AGE = 22;
+    public static final String      USER_CREATION_DATE = "05-26-22";
+    public static final int         USER_NUMBER_GROUPS = 2;
+    public static final String[]    USER_HOBBY = {"Rafting", "Baseball"};
+    public static final String      USER_EMAIL_ADDRESS = "jsmith00@gmail.com";
+    public static final boolean     USER_HAS_AGREED_TO_TERMS = true;
+    public static final boolean     USER_PROFILE_IS_PUBLIC = false;
+    public static final String[]    USER_FAVORITE_GROUPS = {"ColumbiaFireFliesWatchers", "River-Rafters of Columbia"};
+    public static final String[]    USER_CATEGORIES_OF_INTEREST = {"sports", "outdoors"};
+    public static final boolean     USER_ADMIN_PRIVILEGES = false;
+    public static final boolean     USER_AUTHOR_PRIVILEGES = true;
+
 }
