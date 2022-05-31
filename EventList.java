40711ad4579085e7f2585639ed7de63221c6c24f
AaronK <alkeys89@gmail.com>
2022-05-31 17:55:38 -0400

    added a few more  classes and fixed some bugs

--- EventList.java
+++ EventList.java
@@ -3,30 +3,30 @@ import java.util.ArrayList;
 
 public class EventList{
 
-    public EventList<Event> events;
-    public ArrrayList<EventList> eventList;
+    public ArrayList<Event> events;
+    public ArrayList<EventList> eventList;
 
 
     public Event getInstance(){
-
+        return events.get(0);
     }
     public ArrayList<Event> getEvents(){
-
+        return events;
     }
     public void addEvent(Event event){
 
     }
     public Event editEvent(Event event) {
-
+        return events.get(0);
     }
     public Event removeEvent(Event event){
-
+        return events.get(0);
     }
     public Event searchForEvent(Event Event){
-
+        return events.get(0);
     }
     public ArrayList<Event> getFavoriteEvent(){
-
+        return events;
     }
     public void addFavoriteEvent(Event event){
         
