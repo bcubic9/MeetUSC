f5e16468782dd97802c57f5590c730f702dcd6db
Rprentice20 <70124893+Rprentice20@users.noreply.github.com>
2022-06-01 12:08:22 -0400

    Add files via upload

---
+++ UserList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class UserList{
+public UserList userList;
+public ArrayList<User> users;
+public ArrayList<User> tempBannedUsers;
+public ArrayList<User> bannedUsers;
+    public UserList userList(){
+
+    }
+    public UserList getInstance(){
+        return null;
+    }
+    public ArrayList<User> getAllUsers(){
+        return users
+    }
+    private void addUser(){
+
+    }
+}
