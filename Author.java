52a80eccd409894094948133210e463162f206dd
Rprentice20 <70124893+Rprentice20@users.noreply.github.com>
2022-06-01 11:09:23 -0400

    Update Author.java

--- Author.java
+++ Author.java
@@ -1,3 +1,13 @@
 public class Author {
+    public ArrayList<event> myEvents;
+    private void addEvents(){
     
+    }
+    private void editTitle(){
+ 
+    }    
+    private void addCategory()
+    {
+
+    }
 }
