2e07ac611cd02235c683dd4c174c80a61015601d
Denise Garcia Mesa <DG27@email.sc.edu>
2022-06-01 13:46:53 -0400

    added constructors to Group

--- Group.java
+++ Group.java
@@ -1,9 +1,10 @@
 
 
-//import java.lang.reflect.Array;
-import java.util.ArrayList;
 
-public class Group {
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Group{
 
     private ArrayList<Category> category;
     private ArrayList<User> members;
@@ -16,10 +17,32 @@ public class Group {
     private ArrayList<String> chat;
     private User authorContact;
     private ArrayList<Message> messages;
+    private UUID id;
+
+    
+    public Group(String name){
+
+        id = UUID.randomUUID();
+      
+        this.name = name;
+   
+
+    }
+    private Group(UUID id, String name, String shortDescription, ArrayList<Category> categories, ArrayList<User> members, ArrayList<Event> events){
+        this.id = id;
+        this.name = name;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+        this.category = category;
+        this.members = members;
+        this.events = events;
+
+    }
 
 
+    public void joinGroup(User member){
 
-    public void joingGroup(User member){
+        
 
     }
 
