b8b9ec50fa08649a62110cedba570a108f51be2d
AaronK <alkeys89@gmail.com>
2022-06-01 19:24:48 -0400

    minor fixes to InPerson.java

--- InPerson.java
+++ InPerson.java
@@ -1,17 +1,22 @@
 import java.util.ArrayList;
 import java.util.Date;
-public class InPerson extends User {
-private Location location;
-private Date eDate;
-private int numOfAttendees;
-private ArrayList<User> eventAttendees = new ArrayList<User>();
-private ArrayList<User> getAttendees(){
-    return eventAttendees;
-}
-private void addAttendee(User  attendee){
 
-}
-public String toString(){
-    return "";
-}
+public class InPerson {
+    private Location location;
+    private Date eDate;
+    private int numOfAttendees;
+    private ArrayList<User> eventAttendees = new ArrayList<User>();
+
+    private InPerson() {
+        
+    }
+    private ArrayList<User> getAttendees(){
+        return eventAttendees;
+    }
+    private void addAttendee(User  attendee){
+
+    }
+    public String toString(){
+        return "";
+    }
 }
