fecb8d9f8acb239d566599218b40a199cc857469
Rprentice20 <70124893+Rprentice20@users.noreply.github.com>
2022-06-04 16:49:58 -0400

    Update UserList.java

--- UserList.java
+++ UserList.java
@@ -1,8 +1,28 @@
-import java.util.List;
+import java.util.ArrayList;
 
 public class UserList {
-    private UserList userList;
+    private UserList userList = null;
     private List<User> users;
     private List<User> tempBannedUsers;
     private List<User> bannedUsers;
+
+    private UserList userList(){
+
+      this.users = new List<>();
+      this.tempBannedUsers = new List<>();
+      this.bannedUsers = new List<>();
+
+    }
+    public UserList getInstance(){
+      if (userList == null){
+        userList = new UserList();
+      }
+      return userList;
+    }
+    public List<User> getAllUsers(){
+        return users;
+    }
+    public void addUser(User user){
+        users.add(user);
+    }
 }
