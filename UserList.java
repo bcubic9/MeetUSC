bd5479bb35e285982f711f77d691cc9dd6f908f0
AaronKeys <alkeys89@gmail.com>
2022-06-08 12:07:04 -0400

    imported uuid for userlist

--- UserList.java
+++ UserList.java
@@ -1,51 +1,54 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class UserList {
-    private static UserList userList = null;
-    private ArrayList<User> users;
-    private ArrayList<User> tempBannedUsers;
-    private ArrayList<User> bannedUsers;
+  private static UserList userList = null;
+  private ArrayList<User> users;
+  private ArrayList<User> tempBannedUsers;
+  private ArrayList<User> bannedUsers;
 
-    private UserList(){
+  private UserList() {
 
-      this.users = new ArrayList<>();
-      this.tempBannedUsers = new ArrayList<>();
-      this.bannedUsers = new ArrayList<>();
+    this.users = new ArrayList<>();
+    this.tempBannedUsers = new ArrayList<>();
+    this.bannedUsers = new ArrayList<>();
 
+  }
+
+  public static UserList getInstance() {
+    if (userList == null) {
+      userList = new UserList();
     }
-    public static UserList getInstance(){
-      if (userList == null){
-        userList = new UserList();
+    return userList;
+  }
+
+  public ArrayList<User> getAllUsers() {
+    return users;
+  }
+
+  public void addUser(User user) {
+    users.add(user);
+  }
+
+  public ArrayList<User> getUsers() {
+    return users;
+  }
+
+  public User getUserByID(UUID userid) {
+    for (User user : users) {
+      if (user.getUserId().equals(userid)) {
+        return user;
       }
-      return userList;
     }
-    public ArrayList<User> getAllUsers(){
-        return users;
-    }
-    public void addUser(User user){
-        users.add(user);
-    }
+    return null;
+  }
 
-    public ArrayList<User> getUsers() {
-        return users;
-    }
-
-    public User getUserByID(UUID userid) {
-      for (User user : users) {
-        if (user.getUserId().equals(userid)) {
-          return user;
-        }
+  public User getUserByUserName(String userName) {
+    for (User user : users) {
+      if (user.getUserName().equals(userName)) {
+        return user;
       }
-      return null;
     }
-
-    public User getUserByUserName(String userName){
-      for (User user : users) {
-        if (user.getUserName().equals(userName)) {
-          return user;
-        }
-      }
-      return null;
-    }
+    return null;
+  }
 }
-
