6f5610dfea8ab2adf9d6c67711ccae3252297340
Denise Garcia Mesa <DG27@email.sc.edu>
2022-06-09 13:44:27 -0400

    JavDocs for methods in GroupList

--- GroupList.java
+++ GroupList.java
@@ -41,11 +41,22 @@ public class GroupList {
     return newGroup;
   }
 
+  /**
+   * The method removes the group from the group list
+   * @param group
+   * @return group
+   */
   public Group removeGroup(Group group) {
     groups.remove(group);
     return group;
   }
 
+
+  /**
+   * The method will search a group by name
+   * @param name
+   * @return the groups that are relevant to the search
+   */
   public ArrayList<Group> searchForGroupByName(String name) {
     ArrayList<Group> relevantGroupName = new ArrayList<>();
     for (Group e : groups) {
@@ -58,6 +69,11 @@ public class GroupList {
     return relevantGroupName;
   }
 
+  /**
+   * created a UUID for the appropriate group
+   * @param ID
+   * @return the group UUID
+   */
   public Group getGroupByUUID(UUID ID) {
     for (Group e : groups) {
       if (e.getGroupId().equals(ID)) {
@@ -66,6 +82,12 @@ public class GroupList {
     }
     return null;
   }
+
+  /**
+   * the method searches group based on categories
+   * @param category
+   * @return groups that are associated with the category searched
+   */
   public ArrayList<Group> searchGroupByCategory(Category category)
   {
     ArrayList<Group> groupCategories = new ArrayList<Group>();
